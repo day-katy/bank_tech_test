adffcd23bebe1cd1e1f45e9ca0394d23aa1204c4
Katy Day <day.katy@gmail.com>
2021-03-08 15:06:20 +0000

    rename from bank statement to bank account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This code runs a bank statement in IRB
-class BankStatement
+class BankAccount
   def print_statement
     'date || credit || debit || balance'
   end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'bank_statement'
+require 'bank_account'
 
-describe BankStatement do
+describe BankAccount do
   describe '#print_statement' do
     it 'prints the date, credit, debit, and balance of a bank statement' do
       expect(subject.print_statement).to eq 'date || credit || debit || balance'
@@ -10,5 +10,8 @@ describe BankStatement do
   end
 
   describe '#deposit' do
+    it 'adds the amount of the deposit to the entire amount in the bank account' do
+
+    end
   end
 end
