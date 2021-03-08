6fd2f5efe9149c6727bc357ab32c67c3a71b08ef
Katy Day <day.katy@gmail.com>
2021-03-08 15:25:41 +0000

    added current balance test, passed test

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,6 +2,12 @@
 
 # This code runs a bank statement in IRB
 class BankAccount
+  attr_accessor :current_balance
+
+  def initialize
+    @current_balance = 0
+  end
+
 
   def print_statement
     'date || credit || debit || balance\n 14/01/2012 || || 500.00 || 2500.00\n 10/01/2012 || 1000.00 || || 1000.00'
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,6 +3,11 @@
 require 'bank_account'
 
 describe BankAccount do
+
+  it 'starts with an initial balance of 0' do
+    expect(subject.current_balance).to eq 0
+  end
+
   describe '#print_statement' do
     it 'prints the date, credit, debit, and balance of a bank statement' do
       expect(subject.print_statement).to eq 'date || credit || debit || balance\n 14/01/2012 || || 500.00 || 2500.00\n 10/01/2012 || 1000.00 || || 1000.00'
