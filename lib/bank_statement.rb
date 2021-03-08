96ea04e2d814ffa06bdb181f1387188127cf94c6
Katy Day <day.katy@gmail.com>
2021-03-08 15:04:27 +0000

    passing rubocop, rspec

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
+# This code runs a bank statement in IRB
 class BankStatement
-
-def print_statement
-  return "date || credit || debit || balance"
-end
-
+  def print_statement
+    'date || credit || debit || balance'
+  end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,15 +1,14 @@
+# frozen_string_literal: true
+
 require 'bank_statement'
 
 describe BankStatement do
-  
-  describe "#print_statement" do
-    it "prints the date, credit, debit, and balance of a bank statement" do
-      expect(subject.print_statement).to eq "date || credit || debit || balance"
+  describe '#print_statement' do
+    it 'prints the date, credit, debit, and balance of a bank statement' do
+      expect(subject.print_statement).to eq 'date || credit || debit || balance'
     end
-  end 
-
-  describe "#deposit" do
-
   end
 
+  describe '#deposit' do
+  end
 end
