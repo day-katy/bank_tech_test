1a359bf3183612983229379c692ca7901f130ad6
Katy Day <day.katy@gmail.com>
2021-03-09 10:39:00 +0000

    splitting BankAccount class into two - new Transactions class

---
+++ lib/transactions.rb
@@ -0,0 +1,4 @@
+# This code runs the transactions for a bank account
+class Transactions 
+
+end
---
+++ spec/transactions_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'transactions'
+
+describe Transactions do
+  describe '#deposit' do
+    it 'adds the amount of the deposit to an empty bank account' do
+      @account.deposit('08/03/2021', 1000.00)
+      expect(@account.current_balance).to eq 1000.0
+    end
+
+    it 'adds the deposit amount to a positive current balance of a bank account' do
+      @account.deposit('08/03/2021', 1000.00)
+      @account.deposit('09/03/2021', 500.00)
+      expect(@account.current_balance).to eq 1500.0
+    end
+  end
+
+  describe '#withdrawal' do
+    it 'subtracts the amount of the deposit from a positive current balance of a bank account' do
+      @account.deposit('08/03/2021', 1000.00)
+      @account.withdrawal('10/03/2021', 500.00)
+      expect(@account.current_balance).to eq 500.0
+    end
+  end
+end
