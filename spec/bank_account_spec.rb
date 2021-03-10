978898057e0585ebec22c9b6ef2ab0aa4a1a679a
Katy Day <day.katy@gmail.com>
2021-03-10 14:41:28 +0000

    tests passing for statement class

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -41,4 +41,27 @@ describe BankAccount do
       expect(@account.current_balance).to eq 500.0
     end
   end
+
+  describe '#statement' do
+    it 'has the date, credit/debit, and balance of a bank statement' do
+      @account.deposit('10/01/2012', 1000)
+      @account.deposit('13/01/2012', 2000)
+      @account.withdrawal('14/01/2012', 500)
+      expect(@account.statement.print_statement).to eq ['date || credit || debit || balance',
+                                              '14/01/2012 || || 500.00 || 2500.00',
+                                              '13/01/2012 || 2000.00 || || 3000.00',
+                                              '10/01/2012 || 1000.00 || || 1000.00']
+    end
+
+    it 'gets the bank statement information from an array of a pushed deposit to bank statement' do
+      @account.deposit('08/03/2021', 1000.00)
+      expect(@account.statement.print_statement).to eq ['date || credit || debit || balance', '08/03/2021 || 1000.00 || || 1000.00']
+    end
+
+    it 'gets the bank statement information from an array of a pushed withdrawal from bank statement' do
+      @account.deposit('08/03/2021', 1000.00)
+      @account.withdrawal('10/03/2021', 500.00)
+      expect(@account.statement.print_statement).to eq ['date || credit || debit || balance', '10/03/2021 || || 500.00 || 500.00',  '08/03/2021 || 1000.00 || || 1000.00']
+    end
+  end
 end
